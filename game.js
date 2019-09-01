2ead159515d00aac734c920e34aa3af61e606f31
Ksenia Yaremenko <44950983+JeskoJuska@users.noreply.github.com>
2019-09-01 14:15:11 +0300

    Almost the end

--- game.js
+++ game.js
@@ -7,18 +7,34 @@ var userClickedPattern = [];
 
 var startGame = false;
 
+var level = 0;
+
 
 function playSound(name) {
   var audio = new Audio("sounds/" + name + ".mp3");
   audio.play();
 }
 
-function animatePress(color){
-  $("#"+color).addClass("pressed");
+function animatePress(color) {
+  $("#" + color).addClass("pressed");
 
-  setTimeout(function () {
-    $("#"+color).removeClass("pressed");
+  setTimeout(function() {
+    $("#" + color).removeClass("pressed");
   }, 100);
+}
+
+
+$(document).keydown(function() {
+  if (startGame) {} else {
+    startGame = true;
+    nextSequence();
+  }
+});
+
+function wrongColor(){
+  gamePattern = [];
+  level = 0;
+  console.log("wrong");
 }
 
 
@@ -26,18 +42,33 @@ function nextSequence() {
   var randomNumber = Math.floor(Math.random() * 4);
   gamePattern.push(buttonColors[randomNumber]);
 
+  $("h1").text("level " + level++);
+
+
   $("#" + buttonColors[randomNumber]).fadeOut(100).fadeIn(100);
-
   playSound(buttonColors[randomNumber]);
-
 }
 
 
 
 $(".btn").click(function() {
+  //chosen color
   var userChosenColor = this.id; //$(this).attr("id");
 
-  userClickedPattern.push(userChosenColor);
+  //press animation
+  animatePress(userChosenColor);
+
+  //sound of chosen button
   playSound(userChosenColor);
-  console.log(userClickedPattern);
+
+  //adding pressed button to memory or end of the game
+  if (userChosenColor != gamePattern[userClickedPattern.length]) {wrongColor();}
+  else {
+    userClickedPattern.push(userChosenColor);
+  }
+
+  if (userClickedPattern.length === gamePattern.length) setTimeout(function () {
+    userClickedPattern = [];
+    nextSequence();
+  }, 1000);
 });
